ea42de051c6507b68f93d558ba4a46de9d04f413
Gunstrix <39348359+Gunstrix@users.noreply.github.com>
2018-06-26 06:43:15 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on("message", message => {
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const sender = message.author();
+  if(message.channel.type === "dm") return;
+  if (!message.content.startsWith(prefix)) return;
+  
+  if(command == "ping") {
+    
+    message.channel.send("Pong!")
+  
+  }
+  
+  });
+  bot.login(process.env.BOT_TOKEN)
